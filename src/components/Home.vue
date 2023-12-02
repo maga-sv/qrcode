c56714ff68481154df2e63cff7d2dd5784c4909e
Muhammadaziz <salohiddinov.mm@gmail.com>
2023-12-03 00:26:17 +0500

    end

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <section class="qrcode">
+
+      <div class="qrcode__form">
+        <a href="#!" class="qrcode__form-link">
+          <img
+            src="@/assets/images/image-qr-code.png"
+            alt=""
+            class="qrcode__form-img"
+          />
+        </a>
+        <h3 class="qrcode__form-title">
+          Improve your front-end skills by building projects
+        </h3>
+        <p class="qrcode__form-subtitle">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </section>
+
+</template>
